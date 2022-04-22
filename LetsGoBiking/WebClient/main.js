504a7f5133fb79eab88bb2bb23f03cb1c59ce408
Hiba <hiba.khayati@etu.unice.fr>
2022-04-22 21:02:00 +0200

    HTML and CSS complete for the main web client page

---
+++ LetsGoBiking/WebClient/main.js
@@ -0,0 +1,14 @@
+var map = new ol.Map({
+    target: 'map', // <-- This is the id of the div in which the map will be built.
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+
+    view: new ol.View({
+        center: ol.proj.fromLonLat([7.0985774, 43.6365619]), // <-- Those are the GPS coordinates to center the map to.
+        zoom: 10 // You can adjust the default zoom.
+    })
+
+});
